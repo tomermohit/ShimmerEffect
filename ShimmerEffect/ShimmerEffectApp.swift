31b9806aaeb9dea4e703b570328f0b23b6af5ddd
tomermohit <mstomer31@gmail.com>
2024-01-31 11:39:17 +0530

    Initial Commit

---
+++ ShimmerEffect/ShimmerEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerEffectApp.swift
+//  ShimmerEffect
+//
+//  Created by ꧁☆༒☬MoʜɩtToɱɘʀ☬༒☆꧂ on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
